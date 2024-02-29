48c5602c6f90e2255080eb32ec74a1fd1c625941
Romeo Iordachescu <romeo.iordachescu@gmail.com>
2024-02-29 20:39:54 +0100

    Esercizio 2 29.02.24

---
+++ esercitazione_02.php
@@ -0,0 +1,61 @@
+<?php
+
+// Traccia 1
+
+// $users = [
+//     ['name' => 'Romeo', 'surname' => 'Iordachescu', 'gender' => 'M'],
+//     ['name' => 'Antonio', 'surname' => 'Schumaher', 'gender' => 'M'],
+//     ['name' => 'Angela', 'surname' => 'Madrina', 'gender' => 'F'],
+//     ['name' => 'Jessica', 'surname' => 'Alba', 'gender' => 'F'],
+//     ['name' => 'Marco', 'surname' => 'Mendoza', 'gender' => 'M'],
+//     ['name' => 'Nata', 'surname' => 'Albot', 'gender' => 'F'],
+//     ['name' => 'Albert', 'surname' => 'Forza', 'gender' => 'M'],
+// ];
+
+//     foreach ($users as $user) {
+//         $salutation = "Buongiorno";
+    
+//         if ($user['gender'] === 'M') {
+//             $salutation .= " Sig. ";
+//         } elseif ($user['gender'] === 'F') {
+//             $salutation .= " Sig.ra ";
+//         }
+    
+//         echo $salutation . $user['name'] . ' ' . $user['surname'] . "\n";
+// }
+
+// Traccia 2
+
+// $arrayNumerico= [1, 8, 3, 16, 5, 12, 7, 8, 9,14];
+// $somma = 0;
+// $conteggio = 0;
+
+// for ($i = 0; $i < count($arrayNumerico); $i++) {
+//   if($arrayNumerico[$i] % 2 == 0){
+//         $somma += $arrayNumerico[$i];
+//         $conteggio++;
+//     }
+// }
+
+// if ($conteggio > 0) {
+//     $mediaNumeriPari = $somma / $conteggio;
+//     echo "La media dei numeri pari è: $mediaNumeriPari\n";
+// }
+
+// Traccia 3
+
+// for ($i = 1; $i <= 100; $i++) {
+  
+//     if($i % 3 === 0 && $i % 5 === 0) {
+//          echo "HACKACADEMY \n";
+//     } else if($i % 5 === 0)  {
+//         echo "JAVASCRIPT \n"; 
+//     } else if($i % 3 === 0) {
+//         echo "PHP \n"; 
+//     } else {
+//         echo $i . "\n";
+//     }
+// }
+
+?>
+
